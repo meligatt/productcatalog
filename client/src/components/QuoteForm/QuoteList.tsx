ec4d14e59f4faa99bbe29ab0ca9af964a7b316f0
Daniela Gattoni <danigattoni@gmail.com>
2020-07-04 15:16:10 +1000

    feat: quote list crud draft

--- client/src/components/QuoteForm/QuoteList.tsx
+++ client/src/components/QuoteForm/QuoteList.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {
   Box,
   Heading,
-  Table
+  Table,
+  Input
 } from 'fannypack'
  
 const QuoteForm = () => {
@@ -14,21 +15,31 @@ const QuoteForm = () => {
           <Table.Row>
             <Table.HeadCell>Nombre producto</Table.HeadCell>
             <Table.HeadCell textAlign="right">Cantidad</Table.HeadCell>
+            <Table.HeadCell textAlign="right">Acciones</Table.HeadCell>
           </Table.Row>
         </Table.Head>
         <Table.Body>
           <Table.Row>
             <Table.Cell>Protector oido adosable a casco black bull</Table.Cell>
-            <Table.Cell textAlign="right">3</Table.Cell>
+            <Table.Cell textAlign="right"><Input type="text" value={3}/></Table.Cell>
+            <Table.Cell>Borrar</Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Stainless steel eye-facewash</Table.Cell>
-            <Table.Cell textAlign="right">4</Table.Cell>
+            <Table.Cell textAlign="right"><Input type="text" value={2}/></Table.Cell>
+            <Table.Cell>Borrar</Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Mascarilla</Table.Cell>
-            <Table.Cell textAlign="right">15</Table.Cell>
+            <Table.Cell textAlign="right"><Input type="text" value={1}/></Table.Cell>
+            <Table.Cell>Borrar</Table.Cell>
           </Table.Row>
+          <Table.Row>
+            <Table.Cell>Mascarilla</Table.Cell>
+            <Table.Cell textAlign="right"><Input type="text" value={4}/></Table.Cell>
+            <Table.Cell>Borrar</Table.Cell>
+          </Table.Row>
+
         </Table.Body>
       </Table>
     </Box>
---
+++ client/src/pages/quote/Quote.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import QuoteForm from '../../components/QuoteForm/QuoteForm/QuoteForm';
+import QuoteList from '../../components/QuoteForm/QuoteForm/QuoteList';
+
+const Quote: React.FunctionComponent = () => {
+  return (
+    <p>quote</p>
+  );
+}
